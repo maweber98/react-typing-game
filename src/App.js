4375da071e56ae480fdaefab29d09f39f54156c0
maweber98 <weberdesignstudio@gmail.com>
2021-03-01 11:11:59 -0500

    styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useRef} from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 const App = () => {
 
@@ -7,7 +8,7 @@ const App = () => {
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
     const [isTimeRunning, setIsTimeRunning] = useState(false)
-    // const [wordCount, setWordCount] = useState(0)
+    const [wordCount, setWordCount] = useState(0)
 
     const textAreaRef = useRef(null)
     
@@ -18,7 +19,10 @@ const App = () => {
     
     const calculateWordCount = (text) => {
         const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
+        console.log('word count: ', wordsArr.filter(word => word !== "").length)
+        return (
+            wordsArr.filter(word => word !== "").length
+        )
     }
     
     const startGame = () => {
@@ -33,6 +37,34 @@ const App = () => {
         setIsTimeRunning(false)
         // setWordCount(calculateWordCount(text))
     }
+
+    const renderTime = ({ timeRemaining, remainingTime }) => {
+        if (timeRemaining === 0) {
+          return <div className="timer">Times up!</div>;
+        }
+      
+        return (
+          <div className="timer">
+            <div className="text">Remaining</div>
+            <div className="value">{remainingTime}</div>
+            <div className="text">seconds</div>
+          </div>
+        );
+    };
+
+    const renderWordCount = ({ timeRemaining }) => {
+        if (timeRemaining === 0) {
+            return <div className="timer">Times up!</div>;
+          }
+        
+          return (
+            <div className="timer">
+              <div className="text">Word</div>
+              <div className="value">{wordCount}</div>
+              <div className="text">Count</div>
+            </div>
+          );
+    };
     
     useEffect(() => {
 
@@ -44,6 +76,10 @@ const App = () => {
             endGame()
         }
     }, [timeRemaining, isTimeRunning])
+
+    useEffect(() => {
+        setWordCount(calculateWordCount(text))
+    }, [text])
     
     return (
         <div className="container">
@@ -51,8 +87,8 @@ const App = () => {
 
             <div className="form-floating">
                 <textarea 
-                    className="form-control" 
-                    placeholder="Leave a comment here" 
+                    className="form-control grad" 
+                    placeholder="Click Start" 
                     id="floatingTextarea2" 
                     ref={textAreaRef}
                     onChange={handleChange}
@@ -62,15 +98,47 @@ const App = () => {
                 >
                 </textarea>
             </div>
-            <h4 className="fs-3 m-3">Time remaining: {timeRemaining}</h4>
-            <button 
-                className="btn btn-primary"
-                onClick={startGame}
-                disabled={isTimeRunning}
-            >
+
+            {/* Test */}
+            <div id = "boxes">      
+            <div id = "leftbox"> 
+                <CountdownCircleTimer
+                    isPlaying={isTimeRunning}
+                    duration={STARTING_TIME}
+                    colors={[["#FF0000", 0.33], ["#F7B801", 0.33], ["#FFFFFF"]]}
+                    onComplete={() => [true, 1000]}
+                    >
+                    {renderTime}
+                </CountdownCircleTimer>
+            </div>  
+              
+            <div id = "middlebox"> 
+                <button 
+                    type="button" 
+                    className="btn btn-secondary btn-lg"
+                    style={{width: "100%"}}
+                    onClick={startGame}
+                    disabled={isTimeRunning}
+                >   
                 Start
-            </button>
-            <h1 className="fs-3 m-3">Word count: {calculateWordCount(text)}</h1>
+                </button>
+            </div> 
+              
+            <div id = "rightbox"> 
+                <div style={{float:"right"}}>
+                <CountdownCircleTimer
+                    isPlaying={isTimeRunning}
+                    duration={STARTING_TIME}
+                    text={text}
+                    colors={[["#FF0000", 0.33], ["#F7B801", 0.33], ["#FFFFFF"]]}
+                    onComplete={() => [true, 1000]}
+                    >
+                    {renderWordCount}
+                </CountdownCircleTimer>
+                </div>
+            </div> 
+        </div> 
+
         </div>
     )
 }
